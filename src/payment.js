105fcdc157d4b9b5a6afe23bf6fd9ec03f98d914
Clifford Usidamen <cliffordusidamen@gmail.com>
2022-01-30 16:37:08 +0100

    Remove personal paystack public key

--- src/payment.js
+++ src/payment.js
@@ -1,6 +1,6 @@
 function payWithPaystack(email, amount, successCallback, errorCallback) {
     let handler = PaystackPop.setup({
-        key: 'pk_test_ec89eb990d322fe43de3927d08d4de52a4165e03', // Replace with your public key
+        key: 'pk_test_****', // Replace with your public key
         email: email,
         amount: amount,
         ref: '' + Math.floor((Math.random() * 1000000000) + 1),
